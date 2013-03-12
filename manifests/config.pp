3a551eec33179287eb34323901527acd72b2524e
Eugene Vilensky <evilensky@gmail.com>
2013-03-12 12:12:44 -0500

    getting closer, though Avalon solrcore won't launch

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,7 +6,7 @@ class solr::config (
   $version             = $solr::params::solr_version,
   $solr_base           = $solr::params::solr_base,
   $solr_home           = $solr::params::solr_home,
-  $solrconfig_xml      = $solr::params::solrconfig_xml,
+  #  $solr_conf           = "$solr::params::solr_home/conf",
   $solr_xml            = $solr::params::solr_xml,
 
 
@@ -14,18 +14,51 @@ class solr::config (
 
   file { 'solr.xml':
     ensure  => file,
+    owner   => $user,
+    group   => $group,
     content => template('solr/solr.xml.erb'),
     path    => "$tomcat_webapps_conf/solr.xml",
     require => Class['solr::install'],
     notify  => Service['tomcat'],
   }
+  
+  file { "$solr_home":
+    ensure  => directory,
+    owner   => $user,
+    group   => $group,
+    require => File['solr.xml'],
+  }
+  ##TODO parameterize
+  #  file { 'solr_conf':
+  #    ensure  => directory,
+  #    owner   => $user,
+  #    group   => $group,
+  #    require => File[$solr_home],
+  #    path    => "$solr_home/conf",
+  #  }
+  #
+  ##TODO parameterize
+  file { 'solr_conf_avalon':
+    ensure  => directory,
+    path    => "$solr_home/avalon",
+    source  => 'puppet:///modules/solr/avalon',
+    owner   => $solr::params::user,
+    group   => $solr::params::group,
+    recurse => true,
+    require => File["$solr_home"],
+    notify  => Service['tomcat'],
+  }
+  ##TODO parameterize
+  file { 'solr_conf_collection1':
+    ensure  => directory,
+    path    => "$solr_home/collection1",
+    source  => 'puppet:///modules/solr/collection1',
+    owner   => $solr::params::user,
+    group   => $solr::params::group,
+    recurse => true,
+    require => File["$solr_home"],
+    notify  => Service['tomcat'],
+  }
 
-  #  file { 'solrconfig.xml':
-  #    ensure  => file,
-  #    content => template('solr/solrconfig.xml.erb'),
-  #    path    => 
-  #
-  #
-  #
 }
 
