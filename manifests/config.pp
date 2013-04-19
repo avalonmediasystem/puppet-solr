9bf8ad6f94beb9dc9bb674895ad45f58bf3e5497
Michael Klein <mbklein@gmail.com>
2013-04-18 23:26:58 -0500

    Add license headers

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,17 @@
+# Copyright 2011-2013, The Trustees of Indiana University and Northwestern
+#   University.  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+# 
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed 
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the 
+#   specific language governing permissions and limitations under the License.
+# ---  END LICENSE_HEADER BLOCK  ---
+
 class solr::config (
   $tomcat_webapps      = $solr::params::tomcat_webapps,
   $tomcat_webapps_conf = $solr::params::tomcat_webapps_conf,
@@ -54,4 +68,3 @@ class solr::config (
   }
 
 }
-
